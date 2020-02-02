3a8d877b29dee53a3364d12c60c7b50efc52b328
Justinas Marozas <justinas.marozas@gmail.com>
2020-02-02 22:19:12 +0000

    add scaffolding for table ddls

--- 2. Data Modelling/projects/1. Data Modelling with PostgreSQL/sql_queries.py
+++ 2. Data Modelling/projects/1. Data Modelling with PostgreSQL/sql_queries.py
@@ -1,26 +1,46 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = """drop table if exists songplays;"""
+user_table_drop = """drop table if exists users;"""
+song_table_drop = """drop table if exits songs;"""
+artist_table_drop = """drop table if exists artists;"""
+time_table_drop = """drop table if exists "time";"""
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+create table if not exists songplays
+(
+
+);
 """)
 
 user_table_create = ("""
+create table if not exists users
+(
+
+);
 """)
 
 song_table_create = ("""
+create table if not exists songs
+(
+
+);
 """)
 
 artist_table_create = ("""
+create table if not exists artists
+(
+
+);
 """)
 
 time_table_create = ("""
+create table if not exists "time"
+(
+
+);
 """)
 
 # INSERT RECORDS
@@ -48,5 +68,17 @@ song_select = ("""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [
+    songplay_table_create,
+    user_table_create,
+    song_table_create,
+    artist_table_create,
+    time_table_create
+]
+drop_table_queries = [
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop
+]
