6e4760717a621fb7adef4fd4d1c44108ec66cc3d
Justinas Marozas <justinas.marozas@gmail.com>
2020-02-02 23:13:37 +0000

    add first version of table schemas

--- 2. Data Modelling/projects/1. Data Modelling with PostgreSQL/sql_queries.py
+++ 2. Data Modelling/projects/1. Data Modelling with PostgreSQL/sql_queries.py
@@ -2,7 +2,7 @@
 
 songplay_table_drop = """drop table if exists songplays;"""
 user_table_drop = """drop table if exists users;"""
-song_table_drop = """drop table if exits songs;"""
+song_table_drop = """drop table if exists songs;"""
 artist_table_drop = """drop table if exists artists;"""
 time_table_drop = """drop table if exists "time";"""
 
@@ -11,35 +11,61 @@ time_table_drop = """drop table if exists "time";"""
 songplay_table_create = ("""
 create table if not exists songplays
 (
-
+    songplay_id int,
+    start_time timestamp,
+    user_id int,
+    level text,  -- enum?
+    song_id text,
+    artist_id text,
+    session_id int,
+    location text,
+    user_agent text
 );
 """)
 
 user_table_create = ("""
 create table if not exists users
 (
-
+    user_id int,
+    first_name text,
+    last_name text,
+    gender text,  -- enum?
+    level text  -- enum?
 );
 """)
 
 song_table_create = ("""
 create table if not exists songs
 (
-
+    song_id text,
+    title text,
+    artist_id text,
+    year int,
+    duration float
 );
 """)
 
 artist_table_create = ("""
 create table if not exists artists
 (
-
+    artist_id text,
+    name text,
+    location text,
+    latitude float,
+    longitude float
 );
 """)
 
 time_table_create = ("""
 create table if not exists "time"
 (
-
+    start_time timestamp,
+    hour int,
+    day int,
+    week int,
+    month int,
+    year int,
+    weekday int
 );
 """)
 
